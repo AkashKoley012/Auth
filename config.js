7293e3affbb5312bfde781ce1f69618b52be99bd
iCoder <akoley012@gmail.com>
2023-02-15 23:19:16 +0530

    push on git

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '86vvbelczxs8x0', // your App ID
+      'clientSecret': 'cPZQimU5rbfHDTaV', // your App Secret
+      'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    }
+  }
